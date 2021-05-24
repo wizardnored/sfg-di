259ad089a659d689c665e443d49d80128f87bb2a
Nored <antoinederon@gmail.com>
2021-05-24 14:22:52 +0200

    Exercice 4 : Dependency Injection Assignment

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,15 +1,23 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
 import guru.springframework.sfgdi.services.PetService;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
     private final PetService petService;
 
-    public String whichPetIsTheBest(){
-        return petService.getPetType();
+    public PetController(@Qualifier("petService") PetService petService) {
+	this.petService = petService;
+    }
+
+    public String whichPetIsTheBest() {
+	return petService.getPetType();
     }
 }
