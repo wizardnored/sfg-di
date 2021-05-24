cdd92e018d70066b091dcba66d0b6ff84bc38e35
Nored <antoinederon@gmail.com>
2021-05-24 22:13:58 +0200

    Component Scan

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.PetService;
 import org.springframework.stereotype.Controller;
+
+import com.springframework.pets.PetService;
 
 /**
  * Created by jt on 12/28/19.
@@ -12,10 +13,10 @@ public class PetController {
     private final PetService petService;
 
     public PetController(PetService petService) {
-        this.petService = petService;
+	this.petService = petService;
     }
 
-    public String whichPetIsTheBest(){
-        return petService.getPetType();
+    public String whichPetIsTheBest() {
+	return petService.getPetType();
     }
 }
